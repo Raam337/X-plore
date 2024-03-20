94a77cad18494565e40b830cff0fa22a2be8d995
Raymond Chibvongodze <raymondmchibs@gmail.com>
2024-03-20 07:10:13 +0000

    updated styling in post component, and added in sections to retrieve data from the input component

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,50 +1,58 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { getData, sendData } from './assets/utils/database'
-import { Input } from './components/Input/Input'
-import { Auth } from './components/Auth/Auth'
-import { Post } from "./components/Post/Post"
 
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { getData, sendData } from './assets/utils/database';
+import { Input } from './components/Input/Input';
+import { Auth } from './components/Auth/Auth';
+import { Post } from './components/Post/Post';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0);
   const [posts, setPosts] = useState([]);
-  const [auth,setAuth] = useState({status:false, login:"Robert", img:"https://placehold.co/50x50"});
-
-  function btnClick() {
-    setCount((count) => count + 1);
-    sendData(count);
-  }
+  const [auth, setAuth] = useState({ status: false, login: 'Robert', img: 'https://placehold.co/50x50' });
 
   useEffect(() => {
     getData((data) => {
       console.log(data);
       setPosts(data);
     });
+  }, []);
 
-  }, [])
 
+  const handlePostSubmit = (postContent) => {
+    const newPost = {
+      content: postContent,
+      author: auth.login,
+      date: new Date().toISOString(),
+    };
+
+    sendData(newPost)
+      .then(() => {
+        setPosts((prevPosts) => [...prevPosts, newPost]);
+      })
+      .catch((error) => {
+        console.error('Error sending post data:', error);
+      });
+  };
 
   return (
     <>
       <div className="container-fluid text-center">
         <div className="row mt-5">
           <div className="col-3 d-flex flex-wrap justify-content-center align-content-between">
-            <Auth auth={auth} authFunction={setAuth}></Auth>
+            <Auth auth={auth} authFunction={setAuth} />
           </div>
           <div className="col">
-            <Input status={auth.status}></Input>
+            <Input status={auth.status} onPostSubmit={handlePostSubmit} />
           </div>
-          <div className="col-3">
-            Column
-          </div>
+          <div className="col-3">Column</div>
         </div>
       </div>
-      <Post />
+      <div className='col'>
+        <Post posts={posts} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- project/src/components/Post/Post.jsx
+++ project/src/components/Post/Post.jsx
@@ -1,35 +1,52 @@
 import "./Post.css"
 import React from 'react'
-//import {MoreVert} from "@material-ui/icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart as solidHeart, faHeart as regularHeart } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 function Post() {
     return (
-        <div className="post">
-            <div className="postWrapper">
-                <div className="postTop"></div>
-                    <div className="postTopLeft"></div>
-                    <img className="postProfileImg" src="./src/assets/profile.png" alt=""/>
-                    <span className="postUsername">Kaya123</span>
-                    <span className="postDate">15 minutes ago</span>
+        <div className="container col-6">
+            <div className="card Post">
+                <div className="card-header postTop">
+                    <div className="row g-2 align-items-center">
+                        <div className="col-auto">
+                            <img className="postProfileimg rounded-circle" src="./src/assets/profile.png" alt=""/>
+                        </div>
+                        <div className="col">
+                            <span className="postUsername">{postData.name}</span>
+                            <span className="d-block postDate">15 minutes ago</span>
+                        </div>
                     </div>
-                    <div className="postTopRight"></div>
-{/* <MoreVert /> */}
-                <div className="postCenter"></div>
-                <span className="postText">Hey, this place is nice & I reccomend</span>
-                <img className="postImg" src="" alt=""/>
-                
-                <div className="postBottom">
-                        <div className="postBottomLeft"></div> 
-                    <img className="likeIcon" src="./src/assets/like.png" alt=""/>
-                    <img className="heartIcon"src="./src/assets/heart.jpg" alt=""/>
-                    <span className="postlikeCounter">50 people like it</span>
-                    
-                    <div className="postBottomRight"></div> 
-                    <span className="postCommentText">10 Comments</span>
+                    <div className="postTopRight">
+                    </div>
                 </div>
-                </div> 
+                <div className="card-body postCenter">
+                    <p className="card-text">{postData.postText}</p>
+                    {postData.imgData && <img className="img-fluid postImg" src={postData.imgData} alt=""/>}
+                </div>
+                <div className="card-footer postBottom">
+                    <div className="postBottomLeft">
+                        <FontAwesomeIcon icon={faThumbsUp} className="likeIcon" />
+                        <span className="icon-text">Like</span>
+                        <FontAwesomeIcon icon={faHeart} className="heartIcon" />
+                        <span className="icon-text">Love</span>
+                        <span className="postlikeCounter icon">{likeCount} people like it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <button type="button" className="btn btn-primary" onClick={handleComment}>10 Comments</button>
+                    </div>
+                    {postData.place && (
+                    <div className="card-footer postBottom">
+                        <span className="postPlace">Place: {postData.place}</span>
+                    </div>
+                )}
+                </div>
+            </div>
+        </div>
 
-    )
+    );
 }
+
 
 export {Post}
